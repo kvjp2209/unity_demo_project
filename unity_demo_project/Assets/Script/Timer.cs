4b46a0d48f7d9b41ea45cd851bfa91b2ebcfdd16
Nguyễn Thế Kiên <90679946+kvjp2209@users.noreply.github.com>
2022-10-08 19:43:06 +0700

    update timer

--- unity_demo_project/Assets/Script/Timer.cs
+++ unity_demo_project/Assets/Script/Timer.cs
@@ -4,33 +4,27 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    #region Fields
 
-    //  timer duration
+    // timer duration
     float totalSeconds = 0;
 
-    //timer execution
+    // timer execution
     float elapsedSeconds = 0;
     bool running = false;
 
-    //suport for Finished property
+    // support for Finished property
     bool started = false;
 
-    public bool Finished
-    {
-        get { return started && running; }
-    }
+    #endregion
 
-    public bool Running
-    {
-        get { return running; }
-    }
+    #region Properties
 
-    void Start()
-    {
-        //create and run timer
-        
-    }
-
+    /// <summary>
+    /// Sets the duration of the timer
+    /// The duration can only be set if the timer isn't currently running
+    /// </summary>
+    /// <value>duration</value>
     public float Duration
     {
         set
@@ -42,9 +36,35 @@ public class Timer : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Gets whether or not the timer has finished running
+    /// This property returns false if the timer has never been started
+    /// </summary>
+    /// <value>true if finished; otherwise, false.</value>
+    public bool Finished
+    {
+        get { return started && !running; }
+    }
+
+    /// <summary>
+    /// Gets whether or not the timer is currently running
+    /// </summary>
+    /// <value>true if running; otherwise, false.</value>
+    public bool Running
+    {
+        get { return running; }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
-        //update timer and check for finished
+        // update timer and check for finished
         if (running)
         {
             elapsedSeconds += Time.deltaTime;
@@ -54,9 +74,17 @@ public class Timer : MonoBehaviour
             }
         }
     }
+
+    /// <summary>
+    /// Runs the timer
+    /// Because a timer of 0 duration doesn't really make sense,
+    /// the timer only runs if the total seconds is larger than 0
+    /// This also makes sure the consumer of the class has actually 
+    /// set the duration to something higher than 0
+    /// </summary>
     public void Run()
     {
-        //only run with valid duration
+        // only run with valid duration
         if (totalSeconds > 0)
         {
             started = true;
@@ -65,4 +93,5 @@ public class Timer : MonoBehaviour
         }
     }
 
+    #endregion
 }
