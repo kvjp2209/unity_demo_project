06d3181cf5dc72b8fbbdfe2c5c09e35d66812a42
Nguyễn Thế Kiên <90679946+kvjp2209@users.noreply.github.com>
2022-10-14 00:18:47 +0700

    update animation

---
+++ unity_demo_project/Assets/Script/TestMove.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        //references for rigid and animator
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y); 
+
+        //FlipTool player when moving
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(2,2,2);
+        } else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-2, 2, 2);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow) && grounded)
+        {
+            Jump(); 
+        }
+
+        anim.SetBool("run", horizontalInput != 0); 
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+            grounded = true;
+    }
+}
