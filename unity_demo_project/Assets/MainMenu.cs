9730c252ea58ed64db98c4b8b2ac407cee1ef776
duongp2c <85700572+duongp2c@users.noreply.github.com>
2022-11-10 13:57:27 +0700

    Update menu and map

---
+++ unity_demo_project/Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
